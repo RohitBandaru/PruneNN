4ad0d02d63e7b90c82e3f8e071476f84116e6f75
Rohit Bandaru <rb696@cornell.edu>
2020-08-23 22:43:43 -0400

    more code clean up, slim pruner notebook. added linting

--- prunenn/adder.py
+++ prunenn/adder.py
@@ -12,7 +12,7 @@ import itertools
 Adds nodes to neural network
 '''
 class Adder(nn.Module):
-  def __init__(self, model, thres = 0.4, function = "var"):
+  def __init__(self, model, thres = 0.4, function= "var"):
         super(Adder, self).__init__()
         self.model = model
         self.to_add = {}
@@ -31,65 +31,65 @@ class Adder(nn.Module):
                     layer.register_forward_hook(self.low_variance_nodes)
             i += 1
 
-  def add(self):
-      i = 0
-      for layer_name, seq_layer in model.pruning_layers._modules.items():
-        if(i < len(model.pruning_layers._modules)-1):
-          layer = seq_layer._modules['0']
-          #print("adding nodes ", layer_name, " ", self.to_add)
-          nodes_to_add = self.to_add[layer_name]
-          np_weights = layer.weight.data.cpu().numpy()
+    def add(self):
+        i = 0
+        for layer_name, seq_layer in model.pruning_layers._modules.items():
+            if(i < len(model.pruning_layers._modules) - 1):
+                layer = seq_layer._modules['0']
 
-          np_weights[nodes_to_add,:, :, :] = np.random.normal(np_weights[nodes_to_add,:, :, :], 0.5*np.std(np_weights[nodes_to_add,:, :, :]))
-          weights_to_add = np_weights[nodes_to_add,:, :, :]
-          weights_to_add = np.random.normal(weights_to_add, 0.5*np.std(weights_to_add))
-          #weights_to_add = 1/10*np.random.randn(weights_to_add.shape[0],weights_to_add.shape[1],weights_to_add.shape[2],weights_to_add.shape[3])
-          np_weights = np.concatenate((np_weights, weights_to_add), axis = 0).astype(np.float)
-          np_weights = np.random.normal(np_weights,0.5*np.std(np_weights))
-          layer.weight = Parameter(torch.from_numpy(np_weights).type(torch.FloatTensor).cuda())
+                nodes_to_add = self.to_add[layer_name]
+                np_weights = layer.weight.data.cpu().numpy()
 
-          num_nodes_to_add = len(nodes_to_add)
-          # increment out features
-          layer.out_channels += num_nodes_to_add
+                original_weights_to_add = np_weights[nodes_to_add, :, :, :]
+                np_weights[nodes_to_add, :, :, :] = np.random.normal(original_weights_to_add, 0.5 * np.std(original_weights_to_add))
+                weights_to_add = np_weights[nodes_to_add, :, :, :]
+                weights_to_add = np.random.normal(weights_to_add, 0.5 * np.std(weights_to_add))
 
-          # increment in features of next layer
-          next_layer = self.next_layer[layer_name]
-          next_layer = next_layer._modules['0']
-          next_layer.in_channels += num_nodes_to_add
+                np_weights = np.concatenate((np_weights, weights_to_add), axis = 0).astype(np.float)
+                np_weights = np.random.normal(np_weights,0.5 * np.std(np_weights))
+                layer.weight = Parameter(torch.from_numpy(np_weights).type(torch.FloatTensor).cuda())
 
-          bias_weights = layer.bias.data.cpu().numpy()
-          bias_weights[nodes_to_add] = np.random.normal(bias_weights[nodes_to_add],0.5*np.std(bias_weights[nodes_to_add]))
-          bias_to_add = bias_weights[nodes_to_add]
-          bias_to_add = np.random.normal(bias_to_add,0.5*np.std(bias_to_add))
-          #bias_to_add = 1/10*np.random.randn(bias_to_add.shape[0])
-          bias_weights = np.concatenate((bias_weights, bias_to_add)).astype(np.float)
-          bias_weights = np.random.normal(bias_weights,0.5*np.std(bias_weights))
-          layer.bias = Parameter(torch.from_numpy(bias_weights).type(torch.FloatTensor).cuda())
+                num_nodes_to_add = len(nodes_to_add)
+                # increment out features
+                layer.out_channels += num_nodes_to_add
 
-          # add dimension to kernels
-          np_weights = next_layer.weight.data.cpu().numpy()
-          np_weights = np.concatenate((np_weights, np_weights[:, nodes_to_add, :, :]), axis = 1)
-          next_layer.weight = Parameter(torch.from_numpy(np_weights).cuda())
+                # increment in features of next layer
+                next_layer = self.next_layer[layer_name]
+                next_layer = next_layer._modules['0']
+                next_layer.in_channels += num_nodes_to_add
 
-          #update batch layer
-          batch_layer = seq_layer._modules['3']
+                bias_weights = layer.bias.data.cpu().numpy()
+                bias_weights[nodes_to_add] = np.random.normal(bias_weights[nodes_to_add], 0.5 * np.std(bias_weights[nodes_to_add]))
+                bias_to_add = bias_weights[nodes_to_add]
+                bias_to_add = np.random.normal(bias_to_add,0.5*np.std(bias_to_add))
 
-          running_mean = batch_layer.running_mean.data.cpu().numpy()
-          running_mean = np.concatenate((running_mean, running_mean[nodes_to_add]))
-          batch_layer.running_mean = torch.from_numpy(running_mean).cuda()
+                bias_weights = np.concatenate((bias_weights, bias_to_add)).astype(np.float)
+                bias_weights = np.random.normal(bias_weights,0.5*np.std(bias_weights))
+                layer.bias = Parameter(torch.from_numpy(bias_weights).type(torch.FloatTensor).cuda())
 
-          batch_weight = batch_layer.weight.data.cpu().numpy()
-          batch_weight = np.concatenate((batch_weight, batch_weight[nodes_to_add]))
-          batch_layer.weight = Parameter(torch.from_numpy(batch_weight).cuda())
+                # add dimension to kernels
+                np_weights = next_layer.weight.data.cpu().numpy()
+                np_weights = np.concatenate((np_weights, np_weights[:, nodes_to_add, :, :]), axis = 1)
+                next_layer.weight = Parameter(torch.from_numpy(np_weights).cuda())
 
-          batch_bias = batch_layer.bias.data.cpu().numpy()
-          batch_bias = np.concatenate((batch_bias, batch_bias[nodes_to_add]))
-          batch_layer.bias = Parameter(torch.from_numpy(batch_bias).cuda())
+                # update batch layer
+                batch_layer = seq_layer._modules['3']
 
-          running_var = batch_layer.running_var.data.cpu().numpy()
-          running_var = np.concatenate((running_var, running_var[nodes_to_add]))
-          batch_layer.running_var = torch.from_numpy(running_var).cuda()
+                running_mean = batch_layer.running_mean.data.cpu().numpy()
+                running_mean = np.concatenate((running_mean, running_mean[nodes_to_add]))
+                batch_layer.running_mean = torch.from_numpy(running_mean).cuda()
 
+                batch_weight = batch_layer.weight.data.cpu().numpy()
+                batch_weight = np.concatenate((batch_weight, batch_weight[nodes_to_add]))
+                batch_layer.weight = Parameter(torch.from_numpy(batch_weight).cuda())
+
+                batch_bias = batch_layer.bias.data.cpu().numpy()
+                batch_bias = np.concatenate((batch_bias, batch_bias[nodes_to_add]))
+                batch_layer.bias = Parameter(torch.from_numpy(batch_bias).cuda())
+
+                running_var = batch_layer.running_var.data.cpu().numpy()
+                running_var = np.concatenate((running_var, running_var[nodes_to_add]))
+                batch_layer.running_var = torch.from_numpy(running_var).cuda()
         i+=1
 
   def forward(self, x):
@@ -100,6 +100,5 @@ class Adder(nn.Module):
         h, w = output.shape[2], output.shape[3]
         # get correlations
         layer_vars = np.apply_over_axes(np.var, output.cpu().detach().numpy(), [0,2,3])
-        #print("average layer var", np.mean(layer_vars),"min layer var", np.min(layer_vars), layer_vars)
         layer_vars = np.abs(layer_vars)
         self.to_add[layer.layer_name] = np.where((layer_vars.ravel() < self.thres))[0]
--- prunenn/models/mnist_net.py
+++ prunenn/models/mnist_net.py
@@ -1,37 +1,44 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+'''
+Neural network for MNIST
+'''
 
 from collections import OrderedDict
 
-class MNIST_Net(nn.Module):
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MNISTNet(nn.Module):
+    '''
+    Neural network for MNIST
+    '''
     def __init__(self):
-        super(MNIST_Net, self).__init__()
+        super(MNISTNet, self).__init__()
         self.pruning_layers = nn.Sequential(OrderedDict([
             ("Layer1",
-                nn.Sequential(
-                nn.Conv2d(1, 10, 5, padding=2),
-                nn.MaxPool2d(2),
-                nn.ReLU(), nn.BatchNorm2d(10))),
+             nn.Sequential(
+                 nn.Conv2d(1, 10, 5, padding=2),
+                 nn.MaxPool2d(2),
+                 nn.ReLU(), nn.BatchNorm2d(10))),
             ("Layer2",
-                nn.Sequential(nn.Conv2d(10, 20, 5, padding=2),
-                nn.MaxPool2d(2),
-                nn.ReLU(), nn.BatchNorm2d(20))),
+             nn.Sequential(
+                 nn.Conv2d(10, 20, 5, padding=2),
+                 nn.MaxPool2d(2),
+                 nn.ReLU(), nn.BatchNorm2d(20))),
             ("Layer3",
-                nn.Sequential(nn.Conv2d(20, 20, 5, padding=2),
-                nn.MaxPool2d(2),
-                nn.ReLU(), nn.BatchNorm2d(20))),
+             nn.Sequential(
+                 nn.Conv2d(20, 20, 5, padding=2),
+                 nn.MaxPool2d(2),
+                 nn.ReLU(), nn.BatchNorm2d(20))),
             ("Layer4",
-                nn.Sequential(nn.Conv2d(20, 20, 5, padding=2),
-                nn.MaxPool2d(2),
-                nn.ReLU(), nn.BatchNorm2d(20)))
-            ]))
-        self.fc1 = nn.Linear(20, 10)
+             nn.Sequential(
+                 nn.Conv2d(20, 20, 5, padding=2),
+                 nn.MaxPool2d(2),
+                 nn.ReLU(), nn.BatchNorm2d(20)))
+        ]))
+        self.fc_layers = nn.Linear(20, 10)
 
     def forward(self, x):
-
         x = self.pruning_layers(x)
         x = x.view(-1, 20)
-        x = self.fc1(x)
-
+        x = self.fc_layers(x)
         return F.log_softmax(x, dim=1)
